65f96b3b5e38b13ffbf9c78cc554f65722038a8e
Slawek Kasprzak <slawomir.kasprzak@oxmedia.pl>
2019-01-30 17:19:59 +0100

    Moved on with HTML markup and styling. Changed large png screenshots to jpg versions

--- js/script.js
+++ js/script.js
@@ -5,13 +5,30 @@ $(function () {
 	//Slick carousel
 	$('.row-carousel').slick({
 		infinite: true,
+		autoplay: false,
 		slidesToShow: 3,
-		slidesToScroll: 1
+		slidesToScroll: 1,
+		nextArrow: '<button class="slick-next slick-arrow"><i class="fa fa-chevron-right"></i></button>',
+		prevArrow: '<button class="slick-prev slick-arrow"><i class="fa fa-chevron-left"></i></button>',
+		responsive: [{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					slidesToShow: 1
+				}
+			}
+		]
 	});
 
 	//Ekko lightbox
-	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+	$(document).on('click', '[data-toggle="lightbox"]', function (event) {
 		event.preventDefault();
 		$(this).ekkoLightbox();
 	});
-  });
+});
